8992f089a6513aedc10f7274028d361a74950c1c
automationhacks <automationhacks@gmail.com>
2023-02-15 08:23:01 +0000

    Updated add method to use backticks

--- src/test/java/io/automationhacks/CalculatorTest.kt
+++ src/test/java/io/automationhacks/CalculatorTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Assertions.*
 class CalculatorTest {
 
     @Test
-    fun add() {
+    fun `Test calculator can add two numbers`() {
         val calculator = Calculator()
         val expected = 10
         assertEquals(expected, calculator.add(5, 5))
